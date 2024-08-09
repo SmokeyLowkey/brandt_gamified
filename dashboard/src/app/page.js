bde288f0b56b60087f371eee0f9c83295a681e78
SmokeyLowkey <yuriykondakov8@gmail.com>
2024-08-08 23:53:18 -0600

    LFS

---
+++ dashboard/src/app/page.js
@@ -0,0 +1,9 @@
+import { Experience } from "@/components/Experience";
+
+export default function Home() {
+  return (
+    <main className="h-screen min-h-screem">
+      <Experience/>
+    </main>
+  );
+}
---
+++ dashboard/src/components/Experience.jsx
@@ -0,0 +1,15 @@
+"use client"
+import { Box, OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+
+export const Experience = () => {
+  return (
+    <Canvas>
+        <OrbitControls/>
+        <Box>
+            <meshBasicMaterial/>
+        </Box>
+    </Canvas>
+  )
+}
